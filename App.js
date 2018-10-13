00d6095580662cca02da5e6dc048f1b09f4738f2
Younghyun Cho <younghyuncho@Younghyuns-MBP.lfytina2.metronetinc.net>
2018-10-13 04:34:41 -0400

    weather app 1.0

--- App.js
+++ App.js
@@ -1,17 +1,52 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
+
+const API_KEY = "f6f330300924cf643e95566988ec3356";
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null,
+    wind: 0,
   };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition( position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          wind: json.wind.speed,
+          isLoaded: true,
+        })
+    })
+  }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, temperature, name, wind } = this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? null : (
+        <StatusBar hidden={true}/>
+        {isLoaded ? (
+          <Weather weatherName={name} temp={Math.ceil(temperature - 273.15)} wind={wind} />
+        ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather!</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -23,6 +58,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
     flex:1,
---
+++ README.md
@@ -0,0 +1,4 @@
+# 10.12.18 Weather app start
+# 10.13.18 Weather app done
+# Tech-stack : React Native, Expo, MaterialCommunityIcons
+# API : openweathermap.org 
